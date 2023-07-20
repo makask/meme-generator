bde0a9c65923a56e079ef8514a51add5e2cebd05
Marko Kask <kaskmarko@gmail.com>
2023-07-20 16:28:43 +0300

    Add text inputs

---
+++ src/components/Meme.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+function Meme() {
+  const [meme, setMeme] = React.useState({
+    topText: '',
+    bottomText: '',
+    randomImage: 'https://i.imgflip.com/1bij.jpg',
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setMeme((prevMeme) => {
+      return {
+        ...prevMeme,
+        [name]: value,
+      };
+    });
+  }
+
+  return (
+    <main>
+      <div className="form">
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Top text"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          className="form-input"
+          placeholder="Bottom text"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
+      </div>
+    </main>
+  );
+}
+
+export default Meme;
